8578a445f1a6cb07f5c4dabfff70ded491e3c609
Ingrid04 <ingridfmelgar@gmail.com>
2021-05-24 22:15:06 -0400

    change name

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -11,9 +11,9 @@ function Contact() {
         <Container fluid>
           <h1 className="display-3">My Contact Details</h1>
           <p className="lead">Here are my contact details </p>
-          <p className="lead">Email : sachin@wynisco.com </p>
-          <p className="lead">Phone : 732 874 0188 </p>
-          <p className="lead">Typical Wake up / Sleep Hours : Other than bootcamp. I will available morning 7AM to 11AM EST via slack or whatsapp  </p>
+          <p className="lead">Email : ingridfmelgar.com </p>
+          <p className="lead">Phone : 917 620 4558 </p>
+          <p className="lead">Typical Wake up / Sleep Hours : Other than bootcamp. I will available morning 9AM to 5PM EST via slack or whatsapp  </p>
         </Container>
       </Jumbotron>
     </div>
